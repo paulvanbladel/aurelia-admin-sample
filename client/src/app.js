7207b58626b1036b60df5fd8252cda39889be321
paul van bladel <paul.vanbladel@gmail.com>
2015-07-10 15:12:17 +0200

    sidebar extracted into component with ref keyword

--- client/src/app.js
+++ client/src/app.js
@@ -1,46 +1,11 @@
 import 'bootstrap';
 import 'bootstrap/css/bootstrap.css!';
 import 'paulvanbladel/rdash-ui/css/rdash.css!';
-import {inject} from 'aurelia-framework';
-import {ObserverLocator} from 'aurelia-framework';
-import {bindable} from 'aurelia-framework';
-import {Storage} from './components/localStorage';
-@inject(ObserverLocator,Storage)
 export class App {
 	
     configureRouter(config, router){
     	this.router = router;
     }
 	
-	mobileView = 992;
-	constructor(observerLocator, localStorage){
-		this.observerLocator = observerLocator;
-		this.localStorage = localStorage;
-	}
-	get width (){
-		return window.innerWidth;
-	}
-
-	toggleSidebar = function(){
-		this.toggle = !this.toggle;
-		this.localStorage.set('toggle',this.toggle)		;
-	}
-	activate(){
-		this.innerWidthsubscription = this.observerLocator.getObserver(this,'width')
-		.subscribe((newValue,oldValue)=>{
-			if (newValue >= this.mobileView) {
-				if ((this.localStorage.get('toggle')) !== 'undefined') {
-					this.toggle = ! this.localStorage.get('toggle') ? false : true;
-				} else {
-					this.toggle = true;
-				}
-			} else {
-				this.toggle = false;
-			}
-      });
-	}
-	deactivate(){
-		console.log("dispose subscription");
-    	this.innerWidthsubscription(); // disposing the subscription
-    }
+	
 }
--- client/src/components/side-bar.js
+++ client/src/components/side-bar.js
@@ -1,9 +1,40 @@
+
 import {inject} from 'aurelia-framework';
-
+import {ObserverLocator} from 'aurelia-framework';
 import {bindable} from 'aurelia-framework';
-
-
+import {Storage} from './localStorage';
+@inject(ObserverLocator,Storage)
 export class SideBar {
-	  @bindable router = null;
-	  
+	@bindable router = null;
+	mobileView = 992;
+	constructor(observerLocator, localStorage){
+		this.observerLocator = observerLocator;
+		this.localStorage = localStorage;
+	}
+	get width (){
+		return window.innerWidth;
+	}
+
+	toggleSidebar = function(){
+		this.toggle = !this.toggle;
+		this.localStorage.set('toggle',this.toggle)		;
+	}
+	activate(){
+		this.innerWidthsubscription = this.observerLocator.getObserver(this,'width')
+		.subscribe((newValue,oldValue)=>{
+			if (newValue >= this.mobileView) {
+				if ((this.localStorage.get('toggle')) !== 'undefined') {
+					this.toggle = ! this.localStorage.get('toggle') ? false : true;
+				} else {
+					this.toggle = true;
+				}
+			} else {
+				this.toggle = false;
+			}
+      });
+	}
+	deactivate(){
+		console.log("dispose subscription");
+    	this.innerWidthsubscription(); // disposing the subscription
+    }
 }
