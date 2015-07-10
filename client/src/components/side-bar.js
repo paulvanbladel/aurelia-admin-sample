f4def5008d092fa5e98831856852fae825a93558
paul van bladel <paul.vanbladel@gmail.com>
2015-07-10 14:39:00 +0200

    undone sidebar extraction

--- client/src/components/side-bar.js
+++ client/src/components/side-bar.js
@@ -1,40 +1,13 @@
 import {inject} from 'aurelia-framework';
-import {ObserverLocator} from 'aurelia-framework';
+
 import {bindable} from 'aurelia-framework';
-import {Storage} from './localStorage';
+
+
 export class SideBar {
-	@bindable router = null;
-	toggle= false;
-	mobileView = 992;
-	constructor(observerLocator, localStorage){
-		this.observerLocator = observerLocator;
-		this.localStorage = localStorage;
-	}
-	get width (){
-		return window.innerWidth;
-	}
+	  @bindable router = null;
+	  @bindable toggleSidebar() ;
 
-	toggleSidebar(){
-		this.toggle = !this.toggle;
-		this.localStorage.set('toggle',this.toggle)		;
-	}
-	activate(){
-		this.innerWidthsubscription = this.observerLocator.getObserver(this,'width')
-		.subscribe((newValue,oldValue)=>{
-			if (newValue >= this.mobileView) {
-				if ((this.localStorage.get('toggle')) !== 'undefined') {
-					this.toggle = ! this.localStorage.get('toggle') ? false : true;
-				} else {
-					this.toggle = true;
-				}
-			} else {
-				this.toggle = false;
-			}
-      });
-	}
-	deactivate(){
-		console.log("dispose subscription");
-    	this.innerWidthsubscription(); // disposing the subscription
-    }
-
+	  toggleSidebar(){
+	  	console.log("in side bar");
+	  }
 }
